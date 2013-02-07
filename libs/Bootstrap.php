eb4595c6a38ab7c743b5bb13521d634cbdbbaa12
Woody Ghsoubi <vipwoody@gmail.com>
2013-02-06 21:21:56 -0500

    imprived bootstrap
    Bootstrap can handle urls with forward slashes and with no arguments,
    redirects to index

--- libs/Bootstrap.php
+++ libs/Bootstrap.php
@@ -4,13 +4,19 @@
 class Bootstrap {
 
 	function __construct() {
-		$url = $_GET['url'];
+		$url = isset($_GET['url']) ? $_GET['url'] : null;
 		$url = rtrim($url, '/');
 		$url = explode ('/', $url);
 
 		//print_r($url);
 
 		//echo $url;
+
+		if (empty($url[0])){
+			require 'controllers/index.php';
+			$controller = new Index();
+			return false;
+		}
 
 		$file = 'controllers/' . $url[0] . '.php';
 		if (file_exists($file)){
