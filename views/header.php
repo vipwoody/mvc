eaedb965a2e49c348221fdaac371e6453f28441b
Woody Ghsoubi <vipwoody@gmail.com>
2013-02-06 21:20:44 -0500

    added real HTML5 markup

--- views/header.php
+++ views/header.php
@@ -1,3 +1,23 @@
+<!doctype html>
+
 <html>
-Header
+<head>
+	<title>Test</title>
+	<link rel="stylesheet" href="public/css/default.css" />
+	<script src="public/js/jquery.js"></script>
+	<script>
+	// $(function(){
+	// 	alert(1);
+	// });
+	</script>
+</head>
 <body>
+
+	<div id="header">
+		header
+		<br />
+		<a href="index">Index</a>
+		<a href="help">Help</a>
+		<a href="login">Login</a>
+	</div>
+<div id="content">
