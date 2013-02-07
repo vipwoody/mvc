45ac3a9dd9e597b303bcbeb417571ba27863effc
Woody Ghsoubi <vipwoody@gmail.com>
2013-02-06 21:20:00 -0500

    better layout for the index and some other pages

---
+++ views/help/index.php
@@ -0,0 +1,6 @@
+<?php require 'views/header.php'; ?>
+
+HELP!!
+
+
+<?php require 'views/footer.php'; ?>
---
+++ views/login/index.php
@@ -0,0 +1,5 @@
+<?php require 'views/header.php'; ?>
+
+login page!
+
+<?php require 'views/footer.php'; ?>
