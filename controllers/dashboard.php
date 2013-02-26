4370c025881e2e93846571aeb070356d8ca16faa
Woody Ghsoubi <vipwoody@gmail.com>
2013-02-26 09:28:58 -0500

    dashboard controller added new functions

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -1,27 +1,46 @@
 <?php
 
-class Dashboard extends Controller{
+class Dashboard extends Controller {
 
-	function __construct(){
+	function __construct() {
 		parent::__construct();
 		Session::init();
 		$logged = Session::get('loggedIn');
-		if($logged == false){
+		if ($logged == false) {
 			Session::destroy();
 			header('location: ../login');
 			exit;
 		}
+	
+		$this->view->js = array('dashboard/js/default.js');
+		
 	}
-
-	function index()
-	{
+	
+	function index() 
+	{	
 		$this->view->render('dashboard/index');
 	}
-
+	
 	function logout()
 	{
 		Session::destroy();
 		header('location: ../login');
 		exit;
 	}
+	
+	function xhrInsert()
+	{
+		$this->model->xhrInsert();
+	}
+	
+	function xhrGetListings()
+	{
+		$this->model->xhrGetListings();
+	}
+	
+	function xhrDeleteListing()
+	{
+		$this->model->xhrDeleteListing();
+	}
+
 }
